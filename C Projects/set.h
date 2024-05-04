d070606faef081f5738a5a725bdb8802a7147346
nirnatanzon <117411766+nirnatanzon@users.noreply.github.com>
2024-05-04 21:28:53 +0300

    Delete C Projects/set.h

--- C Projects/set.h
+++
@@ -1,6 +0,0 @@
-#define  ENLARGE_SIZE 10
-#define SIZE 50
-int get_set(int * pointerSet , int inSize);
-void print_set(int * pointerSet , int containerSize );
-int inside_the_array(int * pointerSet , int inSize , int val);
-

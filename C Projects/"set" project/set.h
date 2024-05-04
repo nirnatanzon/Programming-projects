ec91948373f3f9e7cf8728c2c2ac2202bf6c8baa
nirnatanzon <117411766+nirnatanzon@users.noreply.github.com>
2024-05-04 22:34:03 +0300

    Add files via upload

---
+++ C Projects/"set" project/set.h
@@ -0,0 +1,6 @@
+#define  ENLARGE_SIZE 10
+#define SIZE 50
+int get_set(int * pointerSet , int inSize);
+void print_set(int * pointerSet , int containerSize );
+int inside_the_array(int * pointerSet , int inSize , int val);
+
